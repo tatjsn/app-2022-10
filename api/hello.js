58de7a1609cd7c5c4a9b3ec628d1cf049f5fbf89
Taz Jackson <tatjsn@users.noreply.github.com>
2022-10-28 16:02:41 +0100

    Simplify

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import { readFileSync } from 'fs';
 import { Firestore } from '@google-cloud/firestore';
 
 export default async function handler(req, res) {
@@ -8,16 +7,11 @@ export default async function handler(req, res) {
   const firestore = new Firestore();
   const docRefs = await firestore.collection('posts').listDocuments();
   const docs = await firestore.getAll(...docRefs);
-  const raw = readFileSync(path.join(process.cwd(), 'package.json'), 'utf8');
-  const pkg = JSON.parse(raw);
 
   res.setHeader('Content-Type', 'text/html');
   return res.send(`
 <body>
 <h1>Hello from server</h1>
-<p>cwd: ${process.cwd()}</p>
-<p>LAMBDA_TASK_ROOT: ${process.env.LAMBDA_TASK_ROOT}</p>
-<p>name: ${pkg.name}</p>
 ${ docs.map(doc => doc.data().title).map(str => '<li>' + str).join('') }
 </body>
   `);
