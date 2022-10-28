60bc1db679d333a23e01afee0f6b54a7a0b6bcdf
Taz Jackson <tatjsn@users.noreply.github.com>
2022-10-28 17:56:10 +0100

    Use template

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { Firestore } from '@google-cloud/firestore';
+import { Environment, FileSystemLoader} from 'nunjucks';
 
 export default async function handler(req, res) {
   const keyPath = path.join(process.cwd(), 'secrets', 'firebase.json');
@@ -7,12 +8,12 @@ export default async function handler(req, res) {
   const firestore = new Firestore();
   const docRefs = await firestore.collection('posts').listDocuments();
   const docs = await firestore.getAll(...docRefs);
+  const items = docs.map(doc => doc.data());
+
+  const env = new Environment(new FileSystemLoader(
+    path.join(process.cwd(), 'views'))
+  );
 
   res.setHeader('Content-Type', 'text/html');
-  return res.send(`
-<body>
-<h1>Hello from server</h1>
-${ docs.map(doc => doc.data().title).map(str => '<li>' + str).join('') }
-</body>
-  `);
+  return res.send(env.render('hello.njk', { items }));
 }
