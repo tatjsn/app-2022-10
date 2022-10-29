5e9c2c07dfe9750d833e2587d39463525dc162d2
Taz Jackson <tatjsn@users.noreply.github.com>
2022-10-29 23:44:33 +0100

    Prettify

--- api/hello.js
+++ api/hello.js
@@ -6,13 +6,17 @@ export default async function handler(req, res) {
   const keyPath = path.join(process.cwd(), 'secrets', 'firebase.json');
   process.env.GOOGLE_APPLICATION_CREDENTIALS = keyPath;
   const firestore = new Firestore();
-  const docRefs = await firestore.collection('hooks').listDocuments();
-  const docs = await firestore.getAll(...docRefs);
-  const items = docs.map(doc => doc.data());
+  const query = await firestore
+    .collection('hooks')
+    .orderBy('timestamp', 'desc')
+    .limit(20)
+    .get();
+  const items = query.docs.map(doc => doc.data());
 
   const env = new Environment(new FileSystemLoader(
     path.join(process.cwd(), 'views'))
   );
+  env.addFilter('date', num => new Date(num).toISOString());
 
   res.setHeader('Content-Type', 'text/html');
   return res.send(env.render('hello.njk', { items }));
