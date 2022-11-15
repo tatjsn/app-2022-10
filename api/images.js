9a308537d525f1d139e0a6d3656bdfab923de8a7
Taz Jackson <tatjsn@users.noreply.github.com>
2022-11-15 22:39:27 +0000

    Add content-length

--- api/images.js
+++ api/images.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
   const fetchRes = await fetch(`https://api-data.line.me/v2/bot/message/${messageId}/content`,
     { headers: { 'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}` } }
   );
-  res.setHeader('Cache-Control', 'Cache-Control: public, max-age=31536000, immutable');
   res.setHeader('Content-Type', fetchRes.headers.get('content-type'));
+  res.setHeader('Content-Length', fetchRes.headers.get('content-length'));
+  res.setHeader('Cache-Control', 'Cache-Control: public, max-age=31536000, immutable');
   fetchRes.body.pipe(res);
 }
