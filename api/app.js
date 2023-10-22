faec2fc32443e0fb5617ace4b177380da5745080
Taz Jackson <tatjsn@users.noreply.github.com>
2023-10-22 22:23:44 +0100

    Add per user view

--- api/app.js
+++ api/app.js
@@ -3,6 +3,27 @@ import view from './clients/view.js';
 import auth from './clients/auth.js';
 import firestore from './clients/firestore.js';
 
+async function getPosts(userId) {
+  const query = await firestore()
+    .collection('hooks')
+    .where('source.userId', '==', userId)
+    .orderBy('timestamp', 'desc')
+    .limit(20)
+    .get();
+  return query.docs.map((doc) => doc.data());
+}
+
+async function getReplies(userId) {
+  const query = await firestore()
+    .collection('replies')
+    .where('userId', '==', userId)
+    .orderBy('timestamp', 'desc')
+    .limit(20)
+    .get();
+  return query.docs.map((doc) => doc.data());
+}
+
+
 export default async function handler(req, res) {
   const { token } = cookie.parse(req.headers.cookie);
 
@@ -12,12 +33,41 @@ export default async function handler(req, res) {
     throw new Exception('Unauthorised');
   }
 
-  const query = await firestore()
-    .collection('hooks')
-    .where('type', '==', 'follow')
-    .get();
-  const items = query.docs.map((doc) => doc.data());
+  if (!req.query || !req.query.userId) {
+    const query = await firestore()
+      .collection('hooks')
+      .where('type', '==', 'follow')
+      .get();
+    const items = query.docs.map((doc) => doc.data());
+
+    res.setHeader('Content-Type', 'text/html');
+    res.send(view().render('app.njk', { items }));
+    return;
+  }
+
+  const [rawPosts, rawReplies] = await Promise.all([getPosts(req.query.userId), getReplies(req.query.userId)]);
+
+  const posts = rawPosts.map(post => ({
+    isReply: false,
+    type: post.type,
+    message: post.message,
+    timestamp: post.timestamp,
+  }));
+
+  const replies = rawReplies.map(reply => ({
+    isReply: true,
+    type: 'message',
+    message: {
+      type: 'text',
+      text: reply.message,
+    },
+    timestamp: reply.timestamp,
+  }));
+
+  const rawItems = [...posts, ...replies];
+  rawItems.sort((a, b) => b.timestamp - a.timestamp);
+  const items = rawItems.slice(0, 10).reverse();
 
   res.setHeader('Content-Type', 'text/html');
-  res.send(view().render('app.njk', { items }));
+  res.send(view().render('appUserId.njk', { items, userId: req.query.userId }));
 }
